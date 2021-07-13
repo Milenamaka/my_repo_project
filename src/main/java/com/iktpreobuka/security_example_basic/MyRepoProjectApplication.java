c1d8ca25f04371d145dbfd610bfda97705f698d0
Milenamaka <milena85.mm@yahoo.com>
2021-07-13 19:52:25 +0200

    init

---
+++ src/main/java/com/iktpreobuka/security_example_basic/MyRepoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.security_example_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRepoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRepoProjectApplication.class, args);
+	}
+
+}
